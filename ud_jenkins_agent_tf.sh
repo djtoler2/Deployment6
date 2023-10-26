123446e0f70bd264a4156949387e24199b75d001
djtoler <84260551+djtoler@users.noreply.github.com>
2023-10-26 04:21:35 -0400

    updated agent ud script

--- ud_jenkins_agent_tf.sh
+++ ud_jenkins_agent_tf.sh
@@ -3,6 +3,9 @@
 sudo apt update
 sudo apt install -y default-jre
 
+sudo apt install build-essential 
+sudo apt install libmysqlclient-dev -y
+
 
 curl -O https://raw.githubusercontent.com/djtoler/automated_installation_scripts/main/auto-terraform.sh
 chmod +x auto-terraform.sh
