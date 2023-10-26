2d2d71e60a8b527f666ff62d4f13b6e1daf3fd7b
djtoler <84260551+djtoler@users.noreply.github.com>
2023-10-26 06:24:51 -0400

    updated db string name

--- database.py
+++ database.py
@@ -53,6 +53,6 @@ class Transactions(Base):
     
 #engine = create_engine('sqlite:///database.db')
 #Base.metadata.create_all(engine)
-DATABASE_URL = "mysql+mysqldb://admin:abcd1234@mydatabase.cq9ye4nsr2ds.us-east-1.rds.amazonaws.com/mydatabase?charset=utf8mb4"
+DATABASE_URL = "mysql+mysqldb://admin:abcd1234@mydatabase.cq9ye4nsr2ds.us-east-1.rds.amazonaws.com/banking?charset=utf8mb4"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=True)
 Base.metadata.create_all(engine)
